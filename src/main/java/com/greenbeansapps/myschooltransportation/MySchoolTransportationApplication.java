baa0d4cabfbe20e8760e251e4a4b4882a567d8a1
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-21 13:54:47 -0300

    primeiro commit

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/MySchoolTransportationApplication.java
@@ -0,0 +1,13 @@
+package com.greenbeansapps.myschooltransportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchoolTransportationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySchoolTransportationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenbeansapps/myschooltransportation/MySchoolTransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenbeansapps.myschooltransportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchoolTransportationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
