34eb962d459912debe2582a4461e31f91ea169be
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-21 14:38:32 -0300

    feat: criando entidade do responsavel

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Responsible.java
@@ -0,0 +1,49 @@
+package com.greenbeansapps.myschooltransportation.domain.entities;
+
+import java.util.UUID;
+
+public class Responsible {
+    private UUID id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Responsible(UUID id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
