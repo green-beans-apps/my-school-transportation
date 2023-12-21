213c9b724552adffe993c8c9af73443f9bcc8aa1
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-21 14:35:34 -0300

    feat: criando entidade de pagamento

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Payment.java
@@ -0,0 +1,12 @@
+package com.greenbeansapps.myschooltransportation.domain.entities;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Payment {
+    private UUID id;
+    private Date paymentDate;
+    private String paymentMonth;
+    private Student student;
+
+}
