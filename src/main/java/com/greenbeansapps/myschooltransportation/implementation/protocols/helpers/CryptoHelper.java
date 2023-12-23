6e3d74617877803dc1f78916ac0cca7b0c72412d
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-22 21:24:16 -0300

    feat: criando helper crypto

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/helpers/CryptoHelper.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/helpers/CryptoHelper.java
@@ -1,2 +1,5 @@
-package com.greenbeansapps.myschooltransportation.implementation.protocols.helpers;public interface CryptoHelper {
+package com.greenbeansapps.myschooltransportation.implementation.protocols.helpers;
+
+public interface CryptoHelper {
+    public String generateRash(String data);
 }
