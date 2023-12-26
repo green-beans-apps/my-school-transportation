8cd8ce102e699ad7afc059e825a141efaff24ce7
Danilo Pereira <danilopereirapessoa@gmail.com>
2023-12-26 16:14:13 -0300

    feat: criando repositorio do endereco

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.greenbeansapps.myschooltransportation.implementation.protocols.repositories;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Address;
+
+public interface AddressRepository {
+    public Address create(Address address);
+}
