d85aab0813d5a269ce0f5730c5d6a7ce98daa330
Danilo Pereira <danilopereirapessoa@gmail.com>
2023-12-27 06:50:07 -0300

    feat: adicionando novo metodo ao repositorio do responsavel

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/ResponsibleRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/ResponsibleRepository.java
@@ -3,9 +3,11 @@ package com.greenbeansapps.myschooltransportation.implementation.protocols.repos
 import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ResponsibleRepository {
     public Responsible create(Responsible responsible);
+    public Optional<Responsible> findById(UUID responsibleId);
     public Optional<Responsible> findByEmail(String email);
     public Optional<Responsible> findByPhoneNumber(String phoneNumber);
 }
