1db5d36275e820958dc4f128400630c041aa77b1
Danilo Pereira <danilopereirapessoa@gmail.com>
2023-12-27 06:25:12 -0300

    feat: criando uma excecao para responsavel invalido

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/exeptions/InvalidResponsibleException.java
@@ -0,0 +1,5 @@
+package com.greenbeansapps.myschooltransportation.domain.exeptions;
+
+public class InvalidResponsibleException extends RuntimeException{
+    public InvalidResponsibleException(){super("Invalid Responsible.");}
+}
