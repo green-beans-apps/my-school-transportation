4fe3da6a5602ef3f3740583ec4eb5c0223039713
Danilo Pereira <danilopereirapessoa@gmail.com>
2023-12-27 06:50:20 -0300

    feat: adicionando novo metodo ao repositorio do endereco

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/AddressRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/AddressRepository.java
@@ -2,6 +2,10 @@ package com.greenbeansapps.myschooltransportation.implementation.protocols.repos
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface AddressRepository {
     public Address create(Address address);
+    public Optional<Address> findById(UUID addressId);
 }
