2cfab523f0fa7b0cef199086a0574255ac49a99b
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-12-30 19:45:36 -0300

    feat: create invalid conductor exception

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/exeptions/InvalidConductorException.java
@@ -0,0 +1,5 @@
+package com.greenbeansapps.myschooltransportation.domain.exeptions;
+
+public class InvalidConductorException extends RuntimeException {
+    public InvalidConductorException(){super("Invalid Conductor Exception.");}
+}
