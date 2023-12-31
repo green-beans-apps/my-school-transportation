21ee1f43aa139d5cf344ade09067e8bb35aecf95
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-12-30 23:18:44 -0300

    feat: criando servico de dominio para criar estudante com endereco e responsavel

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/services/CreateStudentWithAddressAndResponsible.java
@@ -0,0 +1,7 @@
+package com.greenbeansapps.myschooltransportation.domain.services;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Student;
+
+public interface CreateStudentWithAddressAndResponsible {
+    public Student execute();
+}
