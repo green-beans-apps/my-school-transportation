348fd6149681ac7c9faf3ea988b0b5fc0eca57a6
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-12-31 00:45:17 -0300

    refactor: tipando parametro do servico de criar estudante com endereeco e responsavel

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/services/CreateStudentWithAddressAndResponsible.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/services/CreateStudentWithAddressAndResponsible.java
@@ -3,5 +3,18 @@ package com.greenbeansapps.myschooltransportation.domain.services;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
 
 public interface CreateStudentWithAddressAndResponsible {
-    public Student execute();
+    public Student execute(Request request);
+
+  public record Request(
+          StudentData student,
+          ResponsibleData responsible,
+          AddressData address
+  ) { }
+
+  public record StudentData(String studentName, String school, String grade, Integer monthlyPayment, String monthlyPaymentExpiration, String conductorId) { }
+
+  public record ResponsibleData(String responsibleName, String email, String phoneNumber) { }
+
+  public record AddressData(String city, String district, String street, Integer houseNumber) { }
+
 }
