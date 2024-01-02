46a26ed3c4978f3d28ddc51584ba91114de1ad1e
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-02 10:33:22 -0300

    refactor: adicionando metodos na entidade pagamento

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Payment.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Payment.java
@@ -1,12 +1,52 @@
 package com.greenbeansapps.myschooltransportation.domain.entities;
 
+import com.greenbeansapps.myschooltransportation.domain.enums.Months;
+
 import java.util.Date;
 import java.util.UUID;
 
 public class Payment {
     private UUID id;
     private Date paymentDate;
-    private String paymentMonth;
+    private Months paymentMonth;
     private Student student;
 
+    public Payment(UUID id, Date paymentDate, Months paymentMonth, Student student) {
+        this.id = id;
+        this.paymentDate = paymentDate;
+        this.paymentMonth = paymentMonth;
+        this.student = student;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Months getPaymentMonth() {
+        return paymentMonth;
+    }
+
+    public void setPaymentMonth(Months paymentMonth) {
+        this.paymentMonth = paymentMonth;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
