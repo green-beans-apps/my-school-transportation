504ca1643bfba37eb6f6dd6510297a35c5b0934e
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-02 10:36:10 -0300

    feat: criando implementacao do caso de uso de registrar pagamento

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/RegisterPaymentUseCaseImpl.java
@@ -0,0 +1,40 @@
+package com.greenbeansapps.myschooltransportation.implementation.usecases;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Payment;
+import com.greenbeansapps.myschooltransportation.domain.entities.Student;
+import com.greenbeansapps.myschooltransportation.domain.enums.Months;
+import com.greenbeansapps.myschooltransportation.domain.exeptions.ExistingPaymentException;
+import com.greenbeansapps.myschooltransportation.domain.exeptions.StudentNotFoundException;
+import com.greenbeansapps.myschooltransportation.domain.usecases.RegisterPaymentUseCase;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.PaymentRepository;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.StudentRepository;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+public class RegisterPaymentUseCaseImpl implements RegisterPaymentUseCase {
+  private final PaymentRepository paymentRepo;
+  private final StudentRepository studentRepo;
+
+  public RegisterPaymentUseCaseImpl(PaymentRepository paymentRepo, StudentRepository studentRepo) {
+    this.paymentRepo = paymentRepo;
+    this.studentRepo = studentRepo;
+  }
+
+  @Override
+  public Payment execute(UUID studentId, Months paymentMonth) {
+    Optional<Student> student = this.studentRepo.findOneById(studentId);
+    if(student.isEmpty()) {
+      throw new StudentNotFoundException();
+    }
+
+    Optional<Payment> paymentExists = this.paymentRepo.findPaymentPerMonth(studentId, paymentMonth);
+    if(paymentExists.isPresent()) {
+      throw new ExistingPaymentException();
+    }
+
+    Payment newPayment = new Payment(UUID.randomUUID(), new Date(), paymentMonth, student.get());
+    return this.paymentRepo.register(newPayment);
+  }
+}
