c3d42208e16c2029849524f6c67ceee1c7ecb79b
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-02 10:35:01 -0300

    feat: criando caso de uso para registrar pagamento

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/RegisterPaymentUseCase.java
@@ -0,0 +1,10 @@
+package com.greenbeansapps.myschooltransportation.domain.usecases;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Payment;
+import com.greenbeansapps.myschooltransportation.domain.enums.Months;
+
+import java.util.UUID;
+
+public interface RegisterPaymentUseCase {
+  public Payment execute(UUID studentId, Months paymentMonth);
+}
