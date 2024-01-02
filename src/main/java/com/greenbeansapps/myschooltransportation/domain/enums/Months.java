fa348e8a739ed3b2abf6211a6f3de7d4a7bd41e1
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-02 10:33:39 -0300

    feat: criando enum de meses

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/enums/Months.java
@@ -0,0 +1,5 @@
+package com.greenbeansapps.myschooltransportation.domain.enums;
+
+public enum Months {
+  JANEIRO, FEVEREIRO, MARCO, ABRIL, MAIO, JUNHO, JULHO, AGOSTO, SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
+}
