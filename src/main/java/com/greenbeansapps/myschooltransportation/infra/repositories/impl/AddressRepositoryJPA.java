a6a04a91bd33b53a7923fe0b99edacd985b21e35
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-02 20:57:36 -0300

    feat: criando implementacao do repositorio de endereco

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/impl/AddressRepositoryJPA.java
@@ -0,0 +1,36 @@
+package com.greenbeansapps.myschooltransportation.infra.repositories.impl;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Address;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.AddressRepository;
+import com.greenbeansapps.myschooltransportation.infra.repositories.IAddressRepositoryJPA;
+import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.AddressSchema;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class AddressRepositoryJPA implements AddressRepository {
+
+  private final IAddressRepositoryJPA addressRepo;
+
+  public AddressRepositoryJPA(IAddressRepositoryJPA addressRepo) {
+    this.addressRepo = addressRepo;
+  }
+
+  @Override
+  public Address create(Address address) {
+    var newAddress = new AddressSchema();
+    BeanUtils.copyProperties(address, newAddress);
+    this.addressRepo.save(newAddress);
+    return address;
+  }
+
+  @Override
+  public Optional<Address> findById(UUID addressId) {
+    Optional<AddressSchema> addressSchema = this.addressRepo.findById(addressId);
+    if(addressSchema.isEmpty()) {
+      return Optional.empty();
+    }
+    return Optional.of(new Address(addressSchema.get().getId(), addressSchema.get().getCity(), addressSchema.get().getDistrict(), addressSchema.get().getStreet(), addressSchema.get().getHouseNumber()));
+  }
+}
