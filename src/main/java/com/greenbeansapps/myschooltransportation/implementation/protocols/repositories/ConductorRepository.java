e492da26f0455076d0f3de4ef8ec8a393c83aabd
Danilo Pereira <danilopereirapessoa@gmail.com>
2024-01-04 12:20:57 -0300

    refactor: alterando tipo do parametro conductorId no metodo findById

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/ConductorRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/ConductorRepository.java
@@ -3,10 +3,11 @@ package com.greenbeansapps.myschooltransportation.implementation.protocols.repos
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ConductorRepository {
     public Conductor create(Conductor conductor);
     public Optional<Conductor> findByCpf(String cpf);
     public Optional<Conductor> findByEmail(String email);
-    public Optional<Conductor> findById(String email);
+    public Optional<Conductor> findById(UUID conductorId);
 }
