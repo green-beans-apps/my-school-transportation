4e6c02a78dbb86f4eba76df2357d7c567f75b8cf
Danilo Pereira <danilopereirapessoa@gmail.com>
2024-01-04 12:35:12 -0300

    feat: criando schema da entidade responsavel

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/schemas/ResponsibleSchema.java
@@ -0,0 +1,62 @@
+package com.greenbeansapps.myschooltransportation.infra.repositories.schemas;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "responsible_schema")
+public class ResponsibleSchema implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    public ResponsibleSchema() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
