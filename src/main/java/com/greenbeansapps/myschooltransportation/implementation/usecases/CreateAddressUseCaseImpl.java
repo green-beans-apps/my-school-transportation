13a6f8ddd57841e0ca4f9c6d9c0c5090ade5fcfe
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-04 19:47:30 -0300

    refactor: modificando retorno na implementacao do caso de uso de de criar endereco

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/CreateAddressUseCaseImpl.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/CreateAddressUseCaseImpl.java
@@ -3,9 +3,11 @@ package com.greenbeansapps.myschooltransportation.implementation.usecases;
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 import com.greenbeansapps.myschooltransportation.domain.usecases.CreateAddressUseCase;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.AddressRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class CreateAddressUseCaseImpl implements CreateAddressUseCase{
     private final AddressRepository addressRepo;
 
@@ -13,11 +15,9 @@ public class CreateAddressUseCaseImpl implements CreateAddressUseCase{
         this.addressRepo = addressRepo;
     }
 
-
     @Override
     public Address execute(String city, String district, String street, Integer houseNumber) {
         var newAddress = new Address(UUID.randomUUID(), city, district, street, houseNumber);
-        this.addressRepo.create(newAddress);
-        return  newAddress;
+        return this.addressRepo.create(newAddress);
     }
 }
