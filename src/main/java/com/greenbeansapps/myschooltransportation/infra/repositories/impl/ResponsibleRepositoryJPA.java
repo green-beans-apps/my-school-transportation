c0d90250365ad4bc74c9172406da44946e1bcc0e
Danilo Pereira <danilopereirapessoa@gmail.com>
2024-01-04 12:37:02 -0300

    feat: criando implementacao do repositorio do responsavel

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/impl/ResponsibleRepositoryJPA.java
@@ -0,0 +1,54 @@
+package com.greenbeansapps.myschooltransportation.infra.repositories.impl;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.ResponsibleRepository;
+import com.greenbeansapps.myschooltransportation.infra.repositories.IResponsibleRepositoryJPA;
+import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.ResponsibleSchema;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class ResponsibleRepositoryJPA implements ResponsibleRepository {
+
+    private final IResponsibleRepositoryJPA responsibleRepo;
+
+    public ResponsibleRepositoryJPA(IResponsibleRepositoryJPA responsibleRepo) {
+        this.responsibleRepo = responsibleRepo;
+    }
+
+    @Override
+    public Responsible create(Responsible responsible) {
+        var newResponsible = new ResponsibleSchema();
+        BeanUtils.copyProperties(responsible, newResponsible);
+        this.responsibleRepo.save(newResponsible);
+        return responsible;
+    }
+
+    @Override
+    public Optional<Responsible> findById(UUID responsibleId) {
+        Optional<ResponsibleSchema> responsibleSchema = this.responsibleRepo.findById(responsibleId);
+        if (responsibleSchema.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(new Responsible(responsibleSchema.get().getId(), responsibleSchema.get().getName(), responsibleSchema.get().getEmail(), responsibleSchema.get().getPhoneNumber()));
+    }
+
+    @Override
+    public Optional<Responsible> findByEmail(String email) {
+        Optional<ResponsibleSchema> responsibleSchema = this.responsibleRepo.findByEmail(email);
+        if (responsibleSchema.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(new Responsible(responsibleSchema.get().getId(), responsibleSchema.get().getName(), responsibleSchema.get().getEmail(), responsibleSchema.get().getPhoneNumber()));
+    }
+
+    @Override
+    public Optional<Responsible> findByPhoneNumber(String phoneNumber) {
+        Optional<ResponsibleSchema> responsibleSchema = this.responsibleRepo.findByPhoneNumber(phoneNumber);
+        if (responsibleSchema.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(new Responsible(responsibleSchema.get().getId(), responsibleSchema.get().getName(), responsibleSchema.get().getEmail(), responsibleSchema.get().getPhoneNumber()));
+    }
+}
