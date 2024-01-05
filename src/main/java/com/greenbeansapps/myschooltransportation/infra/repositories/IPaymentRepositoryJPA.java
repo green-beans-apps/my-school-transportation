13e6b68924853afe88cab0b74e94049abde13f49
Danilo Pereira <danilopereirapessoa@gmail.com>
2024-01-05 12:04:45 -0300

    refactor: adicionando query personalizada ao metodo findPaymentPerMonth

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IPaymentRepositoryJPA.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IPaymentRepositoryJPA.java
@@ -4,10 +4,14 @@ import com.greenbeansapps.myschooltransportation.domain.entities.Payment;
 import com.greenbeansapps.myschooltransportation.domain.enums.Months;
 import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.PaymentSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface IPaymentRepositoryJPA extends JpaRepository<PaymentSchema, UUID> {
-    Optional<PaymentSchema> findPaymentPerMonth(UUID studentId, Months months);
+    @Query("SELECT p FROM PaymentSchema p WHERE p.student.id = :studentId AND p.paymentMonth = :paymentMonth")
+    Optional<PaymentSchema> findPaymentPerMonth(UUID studentId, Months paymentMonth);
 }
