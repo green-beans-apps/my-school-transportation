2939c7575875a496e58f3928e14b9185d50697b9
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-06 13:11:35 -0300

    feat: mudando assinatura do metodo do caso de uso caso de uso de criar condutor

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateConductorUseCase.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateConductorUseCase.java
@@ -2,6 +2,8 @@ package com.greenbeansapps.myschooltransportation.domain.usecases;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
 
+import java.security.NoSuchAlgorithmException;
+
 public interface CreateConductorUseCase {
-    public Conductor execute(String name, String email, String password, String cpf);
+    public Conductor execute(String name, String email, String password, String cpf) throws NoSuchAlgorithmException;
 }
