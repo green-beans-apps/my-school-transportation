a912359c81987a7fe6bb180fb61e10e24469793c
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-06 13:12:26 -0300

    refactor: modificando assinatura do metodo do crypto helper

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/helpers/CryptoHelper.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/helpers/CryptoHelper.java
@@ -1,5 +1,7 @@
 package com.greenbeansapps.myschooltransportation.implementation.protocols.helpers;
 
+import java.security.NoSuchAlgorithmException;
+
 public interface CryptoHelper {
-    public String generateRash(String data);
+    public String generateRash(String data) throws NoSuchAlgorithmException;
 }
