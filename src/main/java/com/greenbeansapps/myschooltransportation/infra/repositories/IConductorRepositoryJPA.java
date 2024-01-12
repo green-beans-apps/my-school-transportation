ca4f95ed6d17658d5a2fa42c243ce29af7d4b3c5
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-11 22:07:33 -0300

    feat: criando novo metodo para o conductorRepositoryJPA

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IConductorRepositoryJPA.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IConductorRepositoryJPA.java
@@ -2,6 +2,7 @@ package com.greenbeansapps.myschooltransportation.infra.repositories;
 
 import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.ConductorSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +10,6 @@ import java.util.UUID;
 public interface IConductorRepositoryJPA extends JpaRepository<ConductorSchema, UUID> {
     Optional<ConductorSchema> findByCpf(String cpf);
     Optional<ConductorSchema> findByEmail(String email);
+
+    UserDetails findBycpf(String cpf);
 }
