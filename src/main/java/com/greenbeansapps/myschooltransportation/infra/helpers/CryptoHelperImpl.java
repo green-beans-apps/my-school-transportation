0cde401df1c0281f9bdd9508d328ec59f9b81f10
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-11 22:05:55 -0300

    refactor: modificando implementacao do crypto helper

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/helpers/CryptoHelperImpl.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/helpers/CryptoHelperImpl.java
@@ -1,6 +1,7 @@
 package com.greenbeansapps.myschooltransportation.infra.helpers;
 
 import com.greenbeansapps.myschooltransportation.implementation.protocols.helpers.CryptoHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.MessageDigest;
@@ -9,20 +10,7 @@ import java.security.NoSuchAlgorithmException;
 @Service
 public class CryptoHelperImpl implements CryptoHelper {
   @Override
-  public String generateRash(String data) throws NoSuchAlgorithmException {
-    MessageDigest digest = MessageDigest.getInstance("SHA-256");
-    byte[] hashBytes = digest.digest(data.getBytes());
-
-    // Converter os bytes do hash para uma representação hexadecimal
-    StringBuilder hexString = new StringBuilder();
-    for (byte b : hashBytes) {
-      String hex = Integer.toHexString(0xFF & b);
-      if (hex.length() == 1) {
-        hexString.append('0');
-      }
-      hexString.append(hex);
-    }
-
-    return hexString.toString();
+  public String generateRash(String data) {
+    return new BCryptPasswordEncoder().encode(data);
   }
 }
