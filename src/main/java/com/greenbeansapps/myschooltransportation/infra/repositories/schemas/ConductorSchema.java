544a0033b82c62d9dee83631954ad5d3266d2600
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-11 22:08:26 -0300

    feat: adicionando novas dependencias do spring no conductorSchema

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/schemas/ConductorSchema.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/schemas/ConductorSchema.java
@@ -7,11 +7,17 @@ import jakarta.persistence.Table;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "conductor_schema")
-public class ConductorSchema implements Serializable {
+public class ConductorSchema implements Serializable, UserDetails {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -22,10 +28,11 @@ public class ConductorSchema implements Serializable {
     private String name;
     @Column(nullable = false)
     private String email;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String cpf;
     @Column(nullable = false)
     private String password;
+
 
     public ConductorSchema() {
     }
@@ -66,7 +73,35 @@ public class ConductorSchema implements Serializable {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.cpf;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
