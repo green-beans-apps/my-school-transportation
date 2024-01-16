b0160754e3f948a3fb700bc035d4167b4f67842f
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-01-16 19:23:58 -0300

    feat: criando exception para mes invalido

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/exeptions/InvalidMonthException.java
@@ -0,0 +1,5 @@
+package com.greenbeansapps.myschooltransportation.domain.exeptions;
+
+public class InvalidMonthException extends RuntimeException {
+    public InvalidMonthException() { super("Invalid Month Exception");}
+}
