53e7dbc503406c5a9562e2f5940a06553eb57a3b
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-15 21:36:56 -0300

    refactor: atualizando classe de configuracao spring security

--- src/main/java/com/greenbeansapps/myschooltransportation/main/security/SecurityConfig.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/main/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                    .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/conductor/register").permitAll()
                     .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
