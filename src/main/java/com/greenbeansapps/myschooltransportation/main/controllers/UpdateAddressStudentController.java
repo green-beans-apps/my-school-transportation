04f2aef0459281d9c77489a281caec8036cab740
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-01-18 11:05:33 -0300

    refactor: adicionando o BindingResult para capturar os erros

--- src/main/java/com/greenbeansapps/myschooltransportation/main/controllers/UpdateAddressStudentController.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/main/controllers/UpdateAddressStudentController.java
@@ -1,12 +1,15 @@
 package com.greenbeansapps.myschooltransportation.main.controllers;
 
 import com.greenbeansapps.myschooltransportation.implementation.usecases.UpdateAddressStudentUseCaseImpl;
+import com.greenbeansapps.myschooltransportation.main.controllers.erros.ErrorResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +25,15 @@ public class UpdateAddressStudentController {
     private UpdateAddressStudentUseCaseImpl updateAddressStudentUseCase;
 
     @PutMapping("/address")
-    public ResponseEntity updateAddressStudent(@RequestBody @Valid UpdateAddressStudentDto updateAddressStudentDto) {
+    public ResponseEntity updateAddressStudent(@RequestBody @Valid UpdateAddressStudentDto updateAddressStudentDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            ErrorResponse errorResponse = new ErrorResponse();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                errorResponse.addError(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
         var updateAddress = this.updateAddressStudentUseCase.execute(updateAddressStudentDto.studentId, updateAddressStudentDto.city, updateAddressStudentDto.district,
                 updateAddressStudentDto.street, updateAddressStudentDto.referencePoint(), updateAddressStudentDto.houseNumber());
 
--- src/main/java/com/greenbeansapps/myschooltransportation/main/controllers/UpdateResponsibleStudentController.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/main/controllers/UpdateResponsibleStudentController.java
@@ -1,12 +1,16 @@
 package com.greenbeansapps.myschooltransportation.main.controllers;
 
 import com.greenbeansapps.myschooltransportation.implementation.usecases.UpdateResponsibleStudentUseCaseImpl;
+import com.greenbeansapps.myschooltransportation.main.controllers.erros.ErrorResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +26,15 @@ public class UpdateResponsibleStudentController {
     private UpdateResponsibleStudentUseCaseImpl updateResponsibleStudentUseCase;
 
     @PutMapping("/responsible")
-    public ResponseEntity updateResponsibleStudent(@RequestBody @Valid UpdateResponsibleStudentDto updateResponsibleStudentDto) {
+    public ResponseEntity updateResponsibleStudent(@RequestBody @Valid UpdateResponsibleStudentDto updateResponsibleStudentDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            ErrorResponse errorResponse = new ErrorResponse();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                errorResponse.addError(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
         var updateResponsible = this.updateResponsibleStudentUseCase.execute(updateResponsibleStudentDto.studentId, updateResponsibleStudentDto.name,
                 updateResponsibleStudentDto.email, updateResponsibleStudentDto.phoneNumber);
         var newResponsible = new UpdateResponsibleStudentResponsiveDto(updateResponsible.getId(), updateResponsible.getName(), updateResponsible.getEmail(),
@@ -30,6 +42,6 @@ public class UpdateResponsibleStudentController {
         return ResponseEntity.status(HttpStatus.OK).body(newResponsible);
     }
 
-    public record UpdateResponsibleStudentDto(@NotNull UUID studentId, @NotBlank String name, @NotBlank String email, @NotBlank String phoneNumber) { }
+    public record UpdateResponsibleStudentDto(@NotNull UUID studentId, @NotBlank String name, @Email String email, @NotBlank String phoneNumber) { }
     public record UpdateResponsibleStudentResponsiveDto(UUID responsibleId, String name, String email, String phoneNumber) { }
 }
