1d5a51310d133642fada931c42c06652750da8d2
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-01-18 11:05:01 -0300

    refactor: alterando o metodo para deletar estudante, endereco e responsavel

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/DeleteStudentUseCaseImpl.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/DeleteStudentUseCaseImpl.java
@@ -1,27 +1,37 @@
 package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
+import com.greenbeansapps.myschooltransportation.domain.entities.Student;
 import com.greenbeansapps.myschooltransportation.domain.exeptions.StudentNotFoundException;
 import com.greenbeansapps.myschooltransportation.domain.usecases.DeleteStudentUseCase;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.AddressRepository;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.ResponsibleRepository;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class DeleteStudentUseCaseImpl implements DeleteStudentUseCase {
     private final StudentRepository studentRepo;
+    private final AddressRepository addressRepo;
+    private final ResponsibleRepository responsibleRepo;
 
-    public DeleteStudentUseCaseImpl(StudentRepository studentRepo) {
+    public DeleteStudentUseCaseImpl(StudentRepository studentRepo, AddressRepository addressRepo, ResponsibleRepository responsibleRepo) {
         this.studentRepo = studentRepo;
+        this.addressRepo = addressRepo;
+        this.responsibleRepo = responsibleRepo;
     }
 
     @Override
     public void execute(UUID studentId) {
-        Boolean deleteStudent = studentRepo.deleteStudent(studentId);
-        if (!deleteStudent) {
+        Optional<Student> getStudent = studentRepo.findById(studentId);
+        if (getStudent.isEmpty()) {
             throw new StudentNotFoundException();
         }
 
-        studentRepo.deleteStudent(studentId);
+        studentRepo.deleteStudent(getStudent.get().getId());
+        addressRepo.deleteAddress(getStudent.get().getAddress().getId());
+        responsibleRepo.deleteResponsible(getStudent.get().getResponsible().getId());
     }
 }
