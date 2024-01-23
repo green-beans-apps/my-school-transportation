d7636548c03ca3a8db6796ac23ee9e575c50668e
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-22 22:27:51 -0300

    refactor: adicionando validacoes na estidade conductor

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Conductor.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Conductor.java
@@ -1,6 +1,15 @@
 package com.greenbeansapps.myschooltransportation.domain.entities;
 
+import com.greenbeansapps.myschooltransportation.domain.exeptions.CpfIsNotValidException;
+import com.greenbeansapps.myschooltransportation.domain.exeptions.InvalidEmailException;
+import com.greenbeansapps.myschooltransportation.domain.exeptions.PasswordIsNotValidException;
+import com.greenbeansapps.myschooltransportation.domain.utils.CpfValidator;
+import com.greenbeansapps.myschooltransportation.domain.utils.EmailValidator;
+
+import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Conductor {
     private UUID id;
@@ -13,11 +22,11 @@ public class Conductor {
     }
 
     public Conductor(UUID id, String name, String email, String cpf, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.cpf = cpf;
-        this.password = password;
+        setId(id);
+        setName(name);
+        setEmail(email);
+        setCpf(cpf);
+        setPassword(password);
     }
 
     public UUID getId() {
@@ -25,6 +34,9 @@ public class Conductor {
     }
 
     public void setId(UUID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
         this.id = id;
     }
 
@@ -33,6 +45,12 @@ public class Conductor {
     }
 
     public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("the name must have more than 3 characters");
+        }
         this.name = name;
     }
 
@@ -41,6 +59,13 @@ public class Conductor {
     }
 
     public void setEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+
+        if (!EmailValidator.execute(email)) {
+            throw new InvalidEmailException();
+        }
         this.email = email;
     }
 
@@ -49,7 +74,18 @@ public class Conductor {
     }
 
     public void setCpf(String cpf) {
-        this.cpf = cpf;
+        if (cpf == null || cpf.isEmpty()) {
+            throw new IllegalArgumentException("Cpf cannot be null or empty");
+        }
+        String cleanedCpf = cpf.replaceAll("[^0-9]", "");
+
+        if (cleanedCpf.length() != 11) {
+            throw new CpfIsNotValidException();
+        }
+        if (!CpfValidator.execute(cleanedCpf)) {
+            throw new CpfIsNotValidException();
+        }
+        this.cpf = cleanedCpf;
     }
 
     public String getPassword() {
@@ -57,6 +93,9 @@ public class Conductor {
     }
 
     public void setPassword(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
         this.password = password;
     }
 }
