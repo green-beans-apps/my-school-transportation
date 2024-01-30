937d78a719b75608c5c14894387cef1ebf89ad28
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-01-30 16:26:27 -0300

    feat: criando exception de Transportation Type Invalid

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/exceptions/InvalidTransportationTypeException.java
@@ -0,0 +1,5 @@
+package com.greenbeansapps.myschooltransportation.domain.exceptions;
+
+public class InvalidTransportationTypeException extends RuntimeException{
+    public InvalidTransportationTypeException() {super("Invalid Transportation Type Exception");}
+}
