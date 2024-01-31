5ee92d9853ff32b913f5e7ccbe56c63d16f7049c
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-30 23:32:23 -0300

    refactor: modificando parametros do caso de uso de criar condutor

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateConductorUseCase.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateConductorUseCase.java
@@ -3,7 +3,8 @@ package com.greenbeansapps.myschooltransportation.domain.usecases;
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
 public interface CreateConductorUseCase {
-    public Conductor execute(String name, String email, String password, String cpf) throws NoSuchAlgorithmException;
+    public Conductor execute(UUID id, String name, String email, String password, String cpf);
 }
