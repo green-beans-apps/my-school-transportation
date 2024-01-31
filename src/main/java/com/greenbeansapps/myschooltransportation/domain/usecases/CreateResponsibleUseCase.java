b42ee00a7348a1bceff34232a9fbad24f54e1dfe
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-31 16:58:07 -0300

    refactor: adicionando id no parametro do caso de uso de criar responsavel

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateResponsibleUseCase.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateResponsibleUseCase.java
@@ -2,6 +2,8 @@ package com.greenbeansapps.myschooltransportation.domain.usecases;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
 
+import java.util.UUID;
+
 public interface CreateResponsibleUseCase {
-    public Responsible execute(String name, String email, String phoneNumber);
+    public Responsible execute(UUID id, String name, String email, String phoneNumber);
 }
