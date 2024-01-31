962872f269e395ab3d218762e85378d397c7ff9e
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-31 16:57:36 -0300

    refactor: adicionando id no parametro do caso de uso de criar endereco

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateAddressUseCase.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/CreateAddressUseCase.java
@@ -2,6 +2,8 @@ package com.greenbeansapps.myschooltransportation.domain.usecases;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 
+import java.util.UUID;
+
 public interface CreateAddressUseCase {
-    public Address execute(String city, String district, String street, String referencePoint, Integer houseNumber);
+    public Address execute(UUID id, String city, String district, String street, String referencePoint, Integer houseNumber);
 }
