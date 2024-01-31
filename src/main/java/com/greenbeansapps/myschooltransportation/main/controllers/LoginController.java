e3a31b52e33685e67bda497a7ad8e9d1a4fe309b
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-31 17:04:42 -0300

    refactor: removendo imports sem uso no login controller

--- src/main/java/com/greenbeansapps/myschooltransportation/main/controllers/LoginController.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/main/controllers/LoginController.java
@@ -13,10 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
