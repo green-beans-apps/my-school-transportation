9082a3e48e635a90115816c0cf752cb7372c6b19
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-01 01:25:48 -0300

    refactor: adicionando novos metodos inseridos na implementação da JPA

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/StudentRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/StudentRepository.java
@@ -2,6 +2,7 @@ package com.greenbeansapps.myschooltransportation.implementation.protocols.repos
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +10,8 @@ import java.util.UUID;
 
 public interface StudentRepository {
     public Student create(Student student);
-    public List<Student> findAllByConductorId(UUID conductorId);
+    public List<StudentProjection> findAllByConductorId(UUID conductorId);
+    public Optional<StudentProjection> findStudentByIdWithoutConductor(UUID studentId);
     public Optional<Student> findById(UUID studentId);
     public Boolean deleteStudent(UUID studentId);
     public Student updateStudent(Student student);
