51fb09a0a5f6708f3b660f37192e7f8b41e1dce6
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-01 02:26:19 -0300

    test: alterando tipo do mock de estudante para dto da projecao do estudante

--- src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetAllStudentsByConductorIdUseCaseImplTest.java
+++ src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetAllStudentsByConductorIdUseCaseImplTest.java
@@ -1,13 +1,16 @@
 package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
 import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
+import com.greenbeansapps.myschooltransportation.domain.enums.Shift;
 import com.greenbeansapps.myschooltransportation.domain.enums.TransportationType;
 import com.greenbeansapps.myschooltransportation.domain.exceptions.InvalidConductorException;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.ConductorRepository;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.StudentRepository;
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -36,10 +39,10 @@ public class GetAllStudentsByConductorIdUseCaseImplTest {
     Conductor mockConductor = new Conductor(UUID.fromString("c487b1aa-e239-4869-82d4-c38f33dd9ba2"), "Danilo P", "danilo@teste.com", "522.151.300-59", "Davi@280411");;;
     Address mockAddress = new Address(UUID.fromString( "99b7d061-1ad2-46de-aad5-9da1376fb572"),"Olinda", "Pernambuco", "Rua São José", "Próximo ao mercado X", 123);;
     Responsible mockResponsible = new Responsible(UUID.fromString("c43b3422-f72a-4c1f-9b99-59b3261e5e3d"), "Maurício Ferraz", "mauricioferraz@teste.com", "(81)97314-8001");
-    Student mockFirstStudent = new Student(UUID.fromString("296c5a89-5a29-4549-bb48-f57ff7f04619"), "Danilo Pereira Pessoa", "Colégio de São José", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA.toString(), 140,
-            "04", "manha", mockConductor, mockResponsible, mockAddress);
-    Student mockSecondStudent = new Student(UUID.fromString("296c5a89-5a29-4549-bb48-f57ff7f04619"), "Ueslei Nogueira", "Colégio ETE Porto Digital", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA.toString(), 140,
-            "02", "tarde", mockConductor, mockResponsible, mockAddress);
+    StudentProjectionDto mockFirstStudent = new StudentProjectionDto(UUID.fromString("296c5a89-5a29-4549-bb48-f57ff7f04619"), "Danilo Pereira Pessoa", "Colégio de São José", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA, Shift.MANHA, 140,
+            "04", mockResponsible, mockAddress);
+    StudentProjectionDto mockSecondStudent = new StudentProjectionDto(UUID.fromString("296c5a89-5a29-4549-bb48-f57ff7f04619"), "Ueslei Nogueira", "Colégio ETE Porto Digital", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA, Shift.MANHA, 140,
+            "02", mockResponsible, mockAddress);
 
     @Test
     @DisplayName("Deve retornar todos os alunos vinculados ao id do condutor")
@@ -53,12 +56,12 @@ public class GetAllStudentsByConductorIdUseCaseImplTest {
 
         //Assertion
         // checando retornos da consulta realizada
-        Student firstStudent = returnStudents.get(0);
+        StudentProjectionDto firstStudent = returnStudents.get(0);
         assertThat(mockFirstStudent)
                 .usingRecursiveComparison()
                 .isEqualTo(firstStudent);
 
-        Student secondStudent = returnStudents.get(1);
+        StudentProjectionDto secondStudent = returnStudents.get(1);
         assertThat(mockSecondStudent)
                 .usingRecursiveComparison()
                 .isEqualTo(secondStudent);
--- src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetStudentByIdUseCaseImplTest.java
+++ src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetStudentByIdUseCaseImplTest.java
@@ -1,12 +1,15 @@
 package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
 import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
+import com.greenbeansapps.myschooltransportation.domain.enums.Shift;
 import com.greenbeansapps.myschooltransportation.domain.enums.TransportationType;
 import com.greenbeansapps.myschooltransportation.domain.exceptions.StudentNotFoundException;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.StudentRepository;
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,14 +34,14 @@ public class GetStudentByIdUseCaseImplTest {
     Conductor mockConductor = new Conductor(UUID.fromString("c487b1aa-e239-4869-82d4-c38f33dd9ba2"), "Danilo P", "danilo@teste.com", "522.151.300-59", "Davi@280411");;;
     Address mockAddress = new Address(UUID.fromString( "99b7d061-1ad2-46de-aad5-9da1376fb572"),"Olinda", "Pernambuco", "Rua São José", "Próximo ao mercado X", 123);;
     Responsible mockResponsible = new Responsible(UUID.fromString("c43b3422-f72a-4c1f-9b99-59b3261e5e3d"), "Maurício Ferraz", "mauricioferraz@teste.com", "(81)97314-8001");
-    Student mockStudent = new Student(UUID.fromString("28305d91-9d9f-4311-b2ec-f6a12f1bcd4e"), "Danilo Pereira Pessoa", "Colégio de São José", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA.toString(), 140,
-            "04", "manha", mockConductor, mockResponsible, mockAddress);
+    StudentProjectionDto mockStudent = new StudentProjectionDto(UUID.fromString("28305d91-9d9f-4311-b2ec-f6a12f1bcd4e"), "Danilo Pereira Pessoa", "Colégio de São José", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA, Shift.MANHA,140,
+            "04", mockResponsible, mockAddress);
 
     @Test
     @DisplayName("Deve retornar um estudante por id")
     void case1() {
         //Arrange
-        Mockito.when(studentRepo.findById(mockStudent.getId())).thenReturn(Optional.of(mockStudent));
+        Mockito.when(studentRepo.findStudentByIdWithoutConductor(mockStudent.getId())).thenReturn(Optional.of(mockStudent));
 
         //Act
         var returnStudent = getStudentByIdUseCase.execute(mockStudent.getId());
@@ -54,7 +57,7 @@ public class GetStudentByIdUseCaseImplTest {
     @DisplayName("Não deve retornar um estudante com id inválido")
     void case2() {
         //Arrange
-        Mockito.when(studentRepo.findById(mockStudent.getId())).thenReturn(Optional.empty());
+        Mockito.when(studentRepo.findStudentByIdWithoutConductor(mockStudent.getId())).thenReturn(Optional.empty());
 
         //Act and Assert
         assertThrows(StudentNotFoundException.class, () -> {
