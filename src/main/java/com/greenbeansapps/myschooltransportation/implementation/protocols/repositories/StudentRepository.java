11450b378470dad39ce98e97edcc1191de3ea00e
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-01 02:18:09 -0300

    refactor: alterando o retorno dos métodos findAllByConductorId e findStudentByIdWithoutConductor

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/StudentRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.greenbeansapps.myschooltransportation.implementation.protocols.repositories;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
 import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
@@ -10,8 +11,8 @@ import java.util.UUID;
 
 public interface StudentRepository {
     public Student create(Student student);
-    public List<StudentProjection> findAllByConductorId(UUID conductorId);
-    public Optional<StudentProjection> findStudentByIdWithoutConductor(UUID studentId);
+    public List<StudentProjectionDto> findAllByConductorId(UUID conductorId);
+    public Optional<StudentProjectionDto> findStudentByIdWithoutConductor(UUID studentId);
     public Optional<Student> findById(UUID studentId);
     public Boolean deleteStudent(UUID studentId);
     public Student updateStudent(Student student);
