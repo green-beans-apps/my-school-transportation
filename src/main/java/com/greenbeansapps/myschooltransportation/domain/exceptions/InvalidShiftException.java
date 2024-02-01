7136f81c428acd037c8107334a3f03bccf47e7ef
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-01-31 23:55:19 -0300

    feat: criando exception para turno invalido

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/exceptions/InvalidShiftException.java
@@ -0,0 +1,5 @@
+package com.greenbeansapps.myschooltransportation.domain.exceptions;
+
+public class InvalidShiftException extends RuntimeException{
+    public InvalidShiftException() {super("Invalid Shift Exception");}
+}
