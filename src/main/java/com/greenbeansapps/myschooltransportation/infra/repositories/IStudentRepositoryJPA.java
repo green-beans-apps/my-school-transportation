449cdd5c1722ed4265ef29e0f7f41d036354f655
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-01 01:23:45 -0300

    refactor: criando query personalizada para retornar os estudantes sem o objeto conductor

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IStudentRepositoryJPA.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IStudentRepositoryJPA.java
@@ -1,13 +1,20 @@
 package com.greenbeansapps.myschooltransportation.infra.repositories;
 
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.StudentSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface IStudentRepositoryJPA extends JpaRepository<StudentSchema, UUID> {
-    List<StudentSchema> findAllByConductorId(UUID conductorId);
+    @Query("SELECT new com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection(s.id, s.name, s.school, s.grade, s.transportationType, s.shift, s.monthlyPayment, s.monthlyPaymentExpiration, s.responsible, s.address) " +
+            "FROM StudentSchema s WHERE s.conductor.id = :conductorId")
+    List<StudentProjection> findAllByConductorId(UUID conductorId);
+    @Query("SELECT new com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection(s.id, s.name, s.school, s.grade, s.transportationType, s.shift, s.monthlyPayment, s.monthlyPaymentExpiration, s.responsible, s.address) " +
+            "FROM StudentSchema s WHERE s.id = :studentId")
+    Optional<StudentProjection> findStudentByIdWithoutConductor(UUID studentId);
     Optional<StudentSchema> findById(UUID studentId);
 }
