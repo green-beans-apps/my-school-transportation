36ac58d9c871e1583467da99473d08fd0113e6cd
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-04 05:32:35 -0300

    refactor: adicionando novos metodos da abstração do PaymentRepository

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/PaymentRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/PaymentRepository.java
@@ -1,12 +1,15 @@
 package com.greenbeansapps.myschooltransportation.implementation.protocols.repositories;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.PaymentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Payment;
 import com.greenbeansapps.myschooltransportation.domain.enums.Months;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository {
   public Payment register(Payment payment);
   public Optional<Payment> findPaymentPerMonth(UUID studentId, Months months);
+  public List<PaymentProjectionDto> findAllPaymentByStudentId(UUID studentId);
 }
