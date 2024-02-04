f37937c1afdf626acd9e30512642c1ebc54b0187
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-04 05:36:07 -0300

    refactor: alterando retorno e implementacao do metodo execute do caso de uso de GetStudentById

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/GetStudentByIdUseCase.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/GetStudentByIdUseCase.java
@@ -1,11 +1,12 @@
 package com.greenbeansapps.myschooltransportation.domain.usecases;
 
 import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
+import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionWithPaymentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
 import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 
 import java.util.UUID;
 
 public interface GetStudentByIdUseCase {
-  public StudentProjectionDto execute(UUID studentId);
+  public StudentProjectionWithPaymentProjectionDto execute(UUID studentId);
 }
--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetStudentByIdUseCaseImpl.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetStudentByIdUseCaseImpl.java
@@ -1,13 +1,18 @@
 package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.PaymentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
+import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionWithPaymentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
 import com.greenbeansapps.myschooltransportation.domain.exceptions.StudentNotFoundException;
 import com.greenbeansapps.myschooltransportation.domain.usecases.GetStudentByIdUseCase;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.PaymentRepository;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.StudentRepository;
 import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,17 +20,26 @@ import java.util.UUID;
 public class GetStudentByIdUseCaseImpl implements GetStudentByIdUseCase {
 
   private final StudentRepository studentRepo;
+  private final PaymentRepository paymentRepo;
 
-  public GetStudentByIdUseCaseImpl(StudentRepository studentRepo) {
+  public GetStudentByIdUseCaseImpl(StudentRepository studentRepo, PaymentRepository paymentRepo) {
     this.studentRepo = studentRepo;
+    this.paymentRepo = paymentRepo;
   }
 
   @Override
-  public StudentProjectionDto execute(UUID studentId) {
+  public StudentProjectionWithPaymentProjectionDto execute(UUID studentId) {
     Optional<StudentProjectionDto> student = this.studentRepo.findStudentByIdWithoutConductor(studentId);
     if(student.isEmpty()) {
       throw new StudentNotFoundException();
     }
-    return student.get();
+
+    List<StudentProjectionWithPaymentProjectionDto> studentProjectionWithPaymentProjectionDtoList = new ArrayList<>();
+
+    List<PaymentProjectionDto> paymentProjectionDtoList = this.paymentRepo.findAllPaymentByStudentId(studentId);
+
+    return new StudentProjectionWithPaymentProjectionDto(student.get().getId(), student.get().getName(), student.get().getSchool(),
+            student.get().getGrade(), student.get().getTransportationType(), student.get().getShift(), student.get().getMonthlyPayment(),
+            student.get().getMonthlyPaymentExpiration(), student.get().getResponsible(), student.get().getAddress(), paymentProjectionDtoList);
   }
 }
