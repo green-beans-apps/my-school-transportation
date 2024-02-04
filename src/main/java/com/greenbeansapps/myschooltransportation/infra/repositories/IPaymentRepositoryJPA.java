949162f9b55dde6e138e87b209063cea46486e53
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-04 05:31:06 -0300

    refactor: criando query personalizada para retornar os pagamentos sem o estudante

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IPaymentRepositoryJPA.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IPaymentRepositoryJPA.java
@@ -2,16 +2,22 @@ package com.greenbeansapps.myschooltransportation.infra.repositories;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Payment;
 import com.greenbeansapps.myschooltransportation.domain.enums.Months;
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.PaymentProjection;
 import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.PaymentSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface IPaymentRepositoryJPA extends JpaRepository<PaymentSchema, UUID> {
     @Query("SELECT p FROM PaymentSchema p WHERE p.student.id = :studentId AND p.paymentMonth = :paymentMonth")
     Optional<PaymentSchema> findPaymentPerMonth(UUID studentId, Months paymentMonth);
+
+    @Query("SELECT new com.greenbeansapps.myschooltransportation.infra.repositories.projection.PaymentProjection(p.id, p.paymentDate, p.paymentMonth) " +
+            "FROM PaymentSchema p WHERE p.student.id = :studentId")
+    public List<PaymentProjection> findAllPaymentByStudentId(UUID studentId);
 }
