8a40935432738b2faf364ddfc7612eda01904c5d
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-04 05:34:28 -0300

    feat: criando dto de retorno para adicionar a projecao de estudante os pagamentos

---
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/dto/StudentProjectionWithPaymentProjectionDto.java
@@ -0,0 +1,26 @@
+package com.greenbeansapps.myschooltransportation.domain.dto;
+
+import com.greenbeansapps.myschooltransportation.domain.entities.Address;
+import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
+import com.greenbeansapps.myschooltransportation.domain.enums.Shift;
+import com.greenbeansapps.myschooltransportation.domain.enums.TransportationType;
+
+import java.util.List;
+import java.util.UUID;
+
+public class StudentProjectionWithPaymentProjectionDto extends StudentProjectionDto{
+    private List<PaymentProjectionDto> payments;
+
+    public StudentProjectionWithPaymentProjectionDto(UUID id, String name, String school, String grade, TransportationType transportationType, Shift shift, Integer monthlyPayment, String monthlyPaymentExpiration, Responsible responsible, Address address, List<PaymentProjectionDto> payments) {
+        super(id, name, school, grade, transportationType, shift, monthlyPayment, monthlyPaymentExpiration, responsible, address);
+        this.payments = payments;
+    }
+
+    public List<PaymentProjectionDto> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<PaymentProjectionDto> payments) {
+        this.payments = payments;
+    }
+}
