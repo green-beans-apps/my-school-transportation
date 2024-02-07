ba0f3b55dec738b544a5d29324b717b9f339e92a
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-07 19:15:55 -0300

    refactor: adicionando metodos de findConductorByIdWithoutPassword e updateConductor na interface do conductorRepository

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/ConductorRepository.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/protocols/repositories/ConductorRepository.java
@@ -1,6 +1,8 @@
 package com.greenbeansapps.myschooltransportation.implementation.protocols.repositories;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.ConductorProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.ConductorProjection;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +12,6 @@ public interface ConductorRepository {
     public Optional<Conductor> findByCpf(String cpf);
     public Optional<Conductor> findByEmail(String email);
     public Optional<Conductor> findById(UUID conductorId);
+    public Optional<ConductorProjectionDto> findConductorByIdWithoutPassword(UUID conductorId);
+    public Conductor updateConductor(Conductor conductor);
 }
