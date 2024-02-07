484b5edb75e07b9c64df258a4a50a251c74bc596
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-07 19:05:24 -0300

    test: adicionando mocks para correção do teste de buscar estudante pelo id

--- src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetStudentByIdUseCaseImplTest.java
+++ src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/GetStudentByIdUseCaseImplTest.java
@@ -1,13 +1,17 @@
 package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
+import com.greenbeansapps.myschooltransportation.domain.dto.PaymentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
+import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionWithPaymentProjectionDto;
 import com.greenbeansapps.myschooltransportation.domain.entities.Address;
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
 import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
+import com.greenbeansapps.myschooltransportation.domain.enums.Months;
 import com.greenbeansapps.myschooltransportation.domain.enums.Shift;
 import com.greenbeansapps.myschooltransportation.domain.enums.TransportationType;
 import com.greenbeansapps.myschooltransportation.domain.exceptions.StudentNotFoundException;
+import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.PaymentRepository;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.StudentRepository;
 import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
 import org.junit.jupiter.api.DisplayName;
@@ -18,8 +22,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,21 +31,29 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GetStudentByIdUseCaseImplTest {
     @Mock
     StudentRepository studentRepo;
+    @Mock
+    PaymentRepository paymentRepo;
+
     @InjectMocks
     GetStudentByIdUseCaseImpl getStudentByIdUseCase;
 
-    Conductor mockConductor = new Conductor(UUID.fromString("c487b1aa-e239-4869-82d4-c38f33dd9ba2"), "Danilo P", "danilo@teste.com", "522.151.300-59", "Davi@280411");;;
     Address mockAddress = new Address(UUID.fromString( "99b7d061-1ad2-46de-aad5-9da1376fb572"),"Olinda", "Pernambuco", "Rua São José", "Próximo ao mercado X", 123);;
     Responsible mockResponsible = new Responsible(UUID.fromString("c43b3422-f72a-4c1f-9b99-59b3261e5e3d"), "Maurício Ferraz", "mauricioferraz@teste.com", "(81)97314-8001");
-    StudentProjectionDto mockStudent = new StudentProjectionDto(UUID.fromString("28305d91-9d9f-4311-b2ec-f6a12f1bcd4e"), "Danilo Pereira Pessoa", "Colégio de São José", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA, Shift.MANHA,140,
-            "04", mockResponsible, mockAddress);
+    List<PaymentProjectionDto> paymentList = new ArrayList<>(
+            List.of(
+                    new PaymentProjectionDto(UUID.fromString("99b7d061-1ad2-46de-aad5-9da1376fb510"), "18", Months.JANEIRO),
+                    new PaymentProjectionDto(UUID.fromString("99b7d061-1ad2-46de-aad5-9da1376fb511"), "18", Months.FEVEREIRO)
+            )
+    );
+    StudentProjectionWithPaymentProjectionDto mockStudent = new StudentProjectionWithPaymentProjectionDto(UUID.fromString("28305d91-9d9f-4311-b2ec-f6a12f1bcd4e"), "Danilo Pereira Pessoa", "Colégio de São José", "3° Ano (Médio)", TransportationType.IDA_E_VOLTA, Shift.MANHA,140,
+            "18", mockResponsible, mockAddress, paymentList);
 
     @Test
     @DisplayName("Deve retornar um estudante por id")
     void case1() {
         //Arrange
         Mockito.when(studentRepo.findStudentByIdWithoutConductor(mockStudent.getId())).thenReturn(Optional.of(mockStudent));
-
+        Mockito.when(paymentRepo.findAllPaymentByStudentId(mockStudent.getId())).thenReturn(paymentList);
         //Act
         var returnStudent = getStudentByIdUseCase.execute(mockStudent.getId());
 
