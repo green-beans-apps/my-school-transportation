c68a0634cdb24e95cae55a5f797b351a692b7982
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-07 19:13:20 -0300

    refactor: adicionando query personalizada para retornar condutor sem senha

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IConductorRepositoryJPA.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IConductorRepositoryJPA.java
@@ -1,7 +1,9 @@
 package com.greenbeansapps.myschooltransportation.infra.repositories;
 
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.ConductorProjection;
 import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.ConductorSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
@@ -10,6 +12,10 @@ import java.util.UUID;
 public interface IConductorRepositoryJPA extends JpaRepository<ConductorSchema, UUID> {
     Optional<ConductorSchema> findByCpf(String cpf);
     Optional<ConductorSchema> findByEmail(String email);
+
+    @Query("SELECT new com.greenbeansapps.myschooltransportation.infra.repositories.projection.ConductorProjection(c.id, c.name, c.email, c.cpf) " +
+            "FROM ConductorSchema c WHERE c.id = :conductorId")
+    Optional<ConductorProjection> findConductorByIdWithoutPassword(UUID conductorId);
 
     UserDetails findBycpf(String cpf);
 }
