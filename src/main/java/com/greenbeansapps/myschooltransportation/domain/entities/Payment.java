55ca800cea8e5ad39949b753cc03441203fc0481
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-08 21:26:16 -0300

    refactor: alterando logica do metodo setId da entidade Payment

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Payment.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Payment.java
@@ -27,9 +27,10 @@ public class Payment {
 
     public void setId(UUID id) {
         if (id == null) {
-            throw new IllegalArgumentException("Id cannot be null");
+            this.id = UUID.randomUUID();
+        } else {
+            this.id = id;
         }
-        this.id = id;
     }
 
     public String getPaymentDate() {
