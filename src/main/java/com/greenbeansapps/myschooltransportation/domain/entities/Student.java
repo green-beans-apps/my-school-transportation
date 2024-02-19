3da5c4d674ae94d373742abb62212175f600e4a6
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-19 11:23:11 -0300

    refactor: adicionando metodo para formatar strings na entidade student

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Student.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/entities/Student.java
@@ -61,7 +61,7 @@ public class Student {
         if (name.length() < 3 ) {
             throw new IllegalArgumentException("The name must have more than 3 characters");
         }
-        this.name = name;
+        this.name = capitalizeWords(name);
     }
 
     public String getSchool() {
@@ -72,7 +72,7 @@ public class Student {
         if (school == null || school.isEmpty()) {
             throw new IllegalArgumentException("School cannot be null or empty");
         }
-        this.school = school;
+        this.school = capitalizeWords(school);
     }
 
     public String getGrade() {
@@ -176,5 +176,23 @@ public class Student {
         }  else {
             throw new InvalidShiftException();
         }
+    }
+
+    public static String capitalizeWords(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        String[] words = input.split("\\s");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                char firstChar = Character.toUpperCase(word.charAt(0));
+                result.append(firstChar).append(word.substring(1)).append(" ");
+            }
+        }
+
+        return result.toString().trim();
     }
 }
