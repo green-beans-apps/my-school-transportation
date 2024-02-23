242f34de999702ea4d8a9ca98d9a669f89607247
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-22 22:36:47 -0300

    refactor: ajustando tipo do parametro do caso de uso de criar responsavel

--- src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/CreateResponsibleUseCaseImpl.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/implementation/usecases/CreateResponsibleUseCaseImpl.java
@@ -2,6 +2,7 @@ package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Responsible;
 import com.greenbeansapps.myschooltransportation.domain.usecases.CreateResponsibleUseCase;
+import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.CreateResponsibleRequest;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.ResponsibleRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,8 @@ public class CreateResponsibleUseCaseImpl implements CreateResponsibleUseCase {
     }
 
     @Override
-    public Responsible execute(UUID id,String name, String email, String phone) {
-        Responsible newResponsible = new Responsible(id, name, email, phone);
+    public Responsible execute(CreateResponsibleRequest data) {
+        Responsible newResponsible = new Responsible(data.id(), data.name(), data.email(), data.phone());
         this.responsibleRepo.create(newResponsible);
         return newResponsible;
     }
