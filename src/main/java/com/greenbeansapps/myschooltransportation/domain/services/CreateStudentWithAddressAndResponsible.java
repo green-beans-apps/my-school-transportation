68c5922d96f47bb9deb3315bd398560531226d96
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-22 22:31:45 -0300

    refactor: ajustando tipo dos parametros do servico de criar estudante com endereco e responsavel

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/services/CreateStudentWithAddressAndResponsible.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/services/CreateStudentWithAddressAndResponsible.java
@@ -2,22 +2,13 @@ package com.greenbeansapps.myschooltransportation.domain.services;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Student;
 import com.greenbeansapps.myschooltransportation.domain.enums.TransportationType;
+import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.CreateAddressRequest;
+import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.CreateResponsibleRequest;
+import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.CreateStudentRequest;
 
 import java.util.UUID;
 
 public interface CreateStudentWithAddressAndResponsible {
-    public Student execute(CreateStudentWithAddressAndResponsibleRequest request);
-
-  public record CreateStudentWithAddressAndResponsibleRequest(
-          StudentData student,
-          ResponsibleData responsible,
-          AddressData address
-  ) { }
-
-  public record StudentData(UUID id, String studentName, String school, String grade, String transportationType, Double monthlyPayment, Integer monthlyPaymentExpiration,  String shift, UUID conductorId) { }
-
-  public record ResponsibleData(UUID id, String responsibleName, String email, String phone) { }
-
-  public record AddressData(UUID id, String city, String district, String street, String referencePoint, String houseNumber) { }
+    public Student execute(CreateStudentRequest studentData, CreateAddressRequest addressData, CreateResponsibleRequest responsibleData);
 
 }
