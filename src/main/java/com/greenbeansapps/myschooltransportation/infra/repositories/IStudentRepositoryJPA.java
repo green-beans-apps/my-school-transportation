1b72ee0d0cf66a7357b84a76acb861faa8eec1ee
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-27 20:31:17 -0300

    refactor: criando querys para buscar todos os estudantes por id ja com pagamentos e buscar um estudante com pagamentos

--- src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IStudentRepositoryJPA.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/infra/repositories/IStudentRepositoryJPA.java
@@ -1,9 +1,12 @@
 package com.greenbeansapps.myschooltransportation.infra.repositories;
 
+import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.StudentWithPayments;
 import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
+import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentWithPaymentProjection;
 import com.greenbeansapps.myschooltransportation.infra.repositories.schemas.StudentSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +20,13 @@ public interface IStudentRepositoryJPA extends JpaRepository<StudentSchema, UUID
             "FROM StudentSchema s WHERE s.id = :studentId")
     Optional<StudentProjection> findStudentByIdWithoutConductor(UUID studentId);
     Optional<StudentSchema> findById(UUID studentId);
+
+    @Query("SELECT DISTINCT s FROM StudentSchema s " +
+            "LEFT JOIN FETCH s.payments " +
+            "WHERE s.conductor.id = :conductorId")
+    List<StudentSchema> findAllByConductorIdWithPayments(@Param("conductorId") UUID conductorId);
+
+    @Query("SELECT s FROM StudentSchema s WHERE  s.id = :studentId")
+    Optional<StudentSchema> getStudentWithPayments(@Param("studentId") UUID studentId);
+
 }
