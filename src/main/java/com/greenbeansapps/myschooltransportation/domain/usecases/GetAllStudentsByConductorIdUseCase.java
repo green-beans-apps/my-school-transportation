739b824bafb1310cdaa2cb68cdc79869a871f2fa
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-27 20:23:37 -0300

    refactor: alterando tipo do retorno do caso de uso de buscar todos os estudantes pelo id do condutor

--- src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/GetAllStudentsByConductorIdUseCase.java
+++ src/main/java/com/greenbeansapps/myschooltransportation/domain/usecases/GetAllStudentsByConductorIdUseCase.java
@@ -1,13 +1,10 @@
 package com.greenbeansapps.myschooltransportation.domain.usecases;
 
-import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionDto;
-import com.greenbeansapps.myschooltransportation.domain.dto.StudentProjectionWithPaymentProjectionDto;
-import com.greenbeansapps.myschooltransportation.domain.entities.Student;
-import com.greenbeansapps.myschooltransportation.infra.repositories.projection.StudentProjection;
+import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.StudentWithPayments;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface GetAllStudentsByConductorIdUseCase {
-    List<StudentProjectionWithPaymentProjectionDto> execute(UUID conductorId);
+    List<StudentWithPayments> execute(UUID conductorId);
 }
