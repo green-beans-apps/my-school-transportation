f604f481e1204568777976ca80a698a160f1dba7
Danilo Pereira <danilopereirapessoa@gmail.com>
2024-11-30 17:27:05 -0300

    refactor: adicionando teste para validar email duplicado e senha invalida

--- src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/CreateConductorUseCaseImplTest.java
+++ src/test/java/com/greenbeansapps/myschooltransportation/implementation/usecases/CreateConductorUseCaseImplTest.java
@@ -1,6 +1,8 @@
 package com.greenbeansapps.myschooltransportation.implementation.usecases;
 
 import com.greenbeansapps.myschooltransportation.domain.entities.Conductor;
+import com.greenbeansapps.myschooltransportation.domain.exceptions.EmailAlreadyRegisteredException;
+import com.greenbeansapps.myschooltransportation.domain.exceptions.PasswordIsNotValidException;
 import com.greenbeansapps.myschooltransportation.domain.usecases.dtos.CreateConductorRequest;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.helpers.CryptoHelper;
 import com.greenbeansapps.myschooltransportation.implementation.protocols.repositories.ConductorRepository;
@@ -15,11 +17,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateConductorUseCaseImplTest {
@@ -49,9 +51,54 @@ public class CreateConductorUseCaseImplTest {
                 .isEqualTo(mockConductor);
 
         Conductor conductorCapture = conductorCaptor.getValue();
-        org.assertj.core.api.Assertions.assertThat(conductorCapture)
+        assertThat(conductorCapture)
                 .usingRecursiveComparison()
                 .isEqualTo(mockConductor);
 
     }
+
+    @Test
+    @DisplayName("Não deve cadastrar um condutor com um e-mail já registrado")
+    void case2() {
+        Mockito.when(conductorRepo.findByEmail(mockConductor.getEmail()))
+                .thenReturn(Optional.of(mockConductor));
+
+        CreateConductorRequest request = new CreateConductorRequest(
+                mockConductor.getId(),
+               mockConductor.getUsername(),
+                mockConductor.getEmail(), // E-mail já existente
+                mockConductor.getPassword(),
+                mockConductor.getCpf()
+        );
+
+        assertThrows(
+                EmailAlreadyRegisteredException.class,
+                () -> createConductorUseCase.execute(request)
+        );
+
+        Mockito.verify(conductorRepo, Mockito.never()).create(Mockito.any(Conductor.class));
+    }
+
+    @Test
+    @DisplayName("Não deve cadastrar um condutor com a senha inferior a 6 caracteres")
+    void case3() {
+        // Senha não válida
+        mockConductor.setPassword("se123");
+
+        CreateConductorRequest request = new CreateConductorRequest(
+                mockConductor.getId(),
+                mockConductor.getUsername(),
+                mockConductor.getEmail(),
+                mockConductor.getPassword(),
+                mockConductor.getCpf()
+        );
+
+        assertThrows(
+                PasswordIsNotValidException.class,
+                () -> createConductorUseCase.execute(request)
+        );
+
+        Mockito.verify(conductorRepo, Mockito.never()).create(Mockito.any(Conductor.class));
+    }
+
 }
